804f529f647003d34254724e942075ad2ea772a0
AL12F13 <leon_melman@hotmail.com>
2015-06-24 10:42:50 -0500

    Final_3
    final

---
+++ js/acciones.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#btnmenu').on('tap',function(){
+		navigatoor.vibrate(1000);
+	});
+	$('btnCONSECUENCIAS').on('tap',function(){
+		navigatoor.vibrate(1000);
+	});
+	$('btnCORTO').on('tap',function(){
+		navigatoor.vibrate(1000);
+	});
+	$('btnLARGO').on('tap',function(){
+		navigatoor.vibrate(1000);
+	});
+	$('btnTRANSTORNOS').on('tap',function(){
+		navigatoor.vibrate(1000);
+	});
+	$('btnTHC').on('tap',function(){
+		navigatoor.vibrate(1000);
+	});
+	}); 
+});
+
